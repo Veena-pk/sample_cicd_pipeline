9ff8eb2885f5bf9a3878e118a389978d676547cb
Veena P K <veenanair007@gmail.com>
2021-12-09 06:40:15 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from wsgiref import simple_server
+from flask import Flask
+
+import os
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    return "Flask app is running."
+
+port = int(os.getenv("PORT", 5001))
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    httpd = simple_server.make_server(host=host, port=port, app=app)
+    httpd.serve_forever()
